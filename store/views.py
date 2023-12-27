0d531fe3a88c8dc3427f1be7466e01da49146e2d
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2023-12-28 00:11:42 +0300

    "updateItem" view

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create your views here.
@@ -30,3 +31,5 @@ def checkout(request):
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
 
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
