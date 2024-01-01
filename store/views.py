d9aa928000ddb42211b56d7021e4409b6c64760a
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-01-01 17:28:48 +0300

    guestOrder() function

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 import json
 import datetime
 from .models import *
-from .utils import cookieCart, cartData
+from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
 
@@ -67,34 +67,7 @@ def processOrder(request):
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
 
     else:
-        print('User is not logged in..')
-
-        print('COOKIES:', request.COOKIES)
-        name = data['form']['name']
-        email = data['form']['email']
-
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create(
-            email=email,
-        )
-        customer.name = name
-        customer.save()
-
-        order = Order.objects.create(
-            customer=customer,
-            complete=False,
-        )
-
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-
-            orderItem = OrderItem.objects.create(
-                product=product,
-                order=order,
-                quantity=item['quantity'],
-            )
+        customer, order = guestOrder(request, data)
 
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
