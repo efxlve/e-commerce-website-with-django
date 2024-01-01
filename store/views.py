ba890d442826578085455aa6097a85d04bac7421
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-01-01 13:33:07 +0300

    shipping form logic

--- store/views.py
+++ store/views.py
@@ -13,7 +13,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
     products = Product.objects.all()
     context = {'products':products, 'cartItems':cartItems}  
@@ -29,7 +29,7 @@ def cart(request):
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
-    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    context = {'items':items, 'order':order, 'cartItems':cartItems, 'shipping':False}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -42,7 +42,7 @@ def checkout(request):
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
-    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    context = {'items':items, 'order':order, 'cartItems':cartItems, 'shipping':False}
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
